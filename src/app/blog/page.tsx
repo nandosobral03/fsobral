255cf3af5fb4d71eee72dd1520baeff93d0b116c
nandosobral03 <nandosobral03@gmail.com>
2024-12-01 15:12:51 -0300

    feat: blog in main poage

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,6 @@
 import Divider from "@/components/common/divider";
 import LargeTitle from "@/components/common/large-title";
-import { posts } from "./posts";
+import { posts, type Post } from "./posts";
 import Link from "next/link";
 import SectionTitle from "@/components/common/section-title";
 import Image from "next/image";
@@ -10,17 +10,25 @@ export default function Home() {
     <>
       <LargeTitle alt="IPSUM">BLOG</LargeTitle>
       <Divider className="my-4" />
-      {posts.map((post, index) => (
-        <Link href={`/blog/${post.slug}`} key={post.slug} className={`mb-6 mx-4 flex md:h-[300px] grow border-[3px] border-foreground group flex-col-reverse ${index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"}`}>
-          <div className={`p-4 group-hover:bg-foreground group-hover:text-background transition-colors grow ${index % 2 === 0 ? "text-left" : "text-right"}`}>
-            <SectionTitle>{post.title}</SectionTitle>
-            <p className="text-lg mb-2">{post.description}</p>
-            <p className="text-sm font-condensed">{post.date}</p>
-          </div>
-          {post.coverImage && <Image src={post.coverImage} alt={post.title} width={1600} height={900} className="md:w-auto md:h-full w-full h-auto" />}
-        </Link>
-      ))}
+      <div className="mx-4 flex flex-col gap-4">
+        {posts.map((post, index) => (
+          <BlogPost post={post} align={index % 2 === 0 ? "left" : "right"} key={post.slug} />
+        ))}
+      </div>
       <Divider />
     </>
   );
 }
+
+export const BlogPost = ({ post, align }: { post: Post; align: "left" | "right" }) => {
+  return (
+    <Link href={`/blog/${post.slug}`} key={post.slug} className={`mb-6 flex lg:h-[300px] grow border-[3px] border-foreground group flex-col-reverse ${align === "left" ? "lg:flex-row" : "lg:flex-row-reverse"}`}>
+      <div className={`p-4 group-hover:bg-foreground group-hover:text-background transition-colors grow ${align === "left" ? "text-left" : "text-right"}`}>
+        <SectionTitle>{post.title}</SectionTitle>
+        <p className="text-lg mb-2">{post.description}</p>
+        <p className="text-sm font-condensed">{post.date}</p>
+      </div>
+      {post.coverImage && <Image src={post.coverImage} alt={post.title} width={1600} height={900} className="lg:w-auto lg:h-full w-full h-auto" />}
+    </Link>
+  );
+};
--- src/components/sections/blog.tsx
+++ src/components/sections/blog.tsx
@@ -1,11 +1,20 @@
+import { BlogPost } from "@/app/blog/page";
+import { posts } from "@/app/blog/posts";
 import SectionDescription from "@/components/common/section-description";
 import SectionTitle from "@/components/common/section-title";
+import Link from "next/link";
 
 export default function AboutMe() {
   return (
     <section className="p-6 flex flex-col gap-4">
-      <SectionTitle>Blog</SectionTitle>
-      <SectionDescription>Occasionally, I write short articles about things I’ve learned or topics I’ve enjoyed exploring.</SectionDescription>
+      <SectionTitle>
+        Blog
+        <Link href="/blog" className="inline-flex">
+          <span className="material-symbols-outlined text-3xl my-auto">north_east</span>
+        </Link>
+      </SectionTitle>
+      <SectionDescription>Occasionally, I write short articles about things I've learned or topics I've enjoyed exploring.</SectionDescription>
+      <BlogPost post={posts[0]} align="left" />
     </section>
   );
 }
