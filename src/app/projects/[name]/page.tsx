c3bb1a6a6bf836ef71e390e3e0f538b388b86b08
nandosobral03 <nandosobral03@gmail.com>
2024-12-03 17:21:13 -0300

    params

--- src/app/projects/[name]/page.tsx
+++ src/app/projects/[name]/page.tsx
@@ -2,9 +2,10 @@ import NotFound from "@/components/sections/NotFound";
 import ProjectDetails from "@/app/projects/project-details";
 import { projects } from "@/app/projects/projects";
 
-export default async function ProjectPage({ params }: { params: { name: string } }) {
+export default async function ProjectPage({ params }: { params: Promise<{ name: string }> }) {
+  const resolvedParams = await params;
   // Replace URL-encoded spaces with regular spaces before comparing
-  const decodedName = decodeURIComponent(params.name);
+  const decodedName = decodeURIComponent(resolvedParams.name);
   const project = projects.find((project) => project.name === decodedName);
 
   if (!project) {
