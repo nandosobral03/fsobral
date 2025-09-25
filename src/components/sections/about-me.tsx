79548bfc76dd9acf50733179aa1aa3e54520f0ad
nandosobral03 <nandosobral03@gmail.com>
2025-09-25 19:16:00 -0300

    fix: current job

--- src/components/sections/about-me.tsx
+++ src/components/sections/about-me.tsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
     <section className="w-1/2 p-6 flex flex-col gap-4">
       <SectionTitle>About Me</SectionTitle>
       <SectionDescription>
-        Hi! I'm a 25-year-old Software Engineer and Full Stack Developer from Uruguay, currently working as a Tech Lead at Eagerworks. I'm passionate about writing code, learning new technologies, and building things.
+        Hi! I'm a Software Engineer and Full Stack Developer from Uruguay, currently working as a Software Engineer at Keeper.app (YC S21). I'm passionate about writing code, learning new technologies, and building things.
       </SectionDescription>
     </section>
   );
