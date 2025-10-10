e1444a2efc9364b64228ba947cb6dbc23cf9c16e
nandosobral03 <nandosobral03@gmail.com>
2025-10-09 21:23:23 -0300

    fix fix

--- src/app/blog/components/blog-post.tsx
+++ src/app/blog/components/blog-post.tsx
@@ -6,17 +6,13 @@ import Tag from "@/components/common/tag";
 import { calculateReadingTime } from "../utils/reading-time";
 
 export const BlogPost = ({ post, align }: { post: Post; align: "left" | "right" }) => {
-  const readingTimeMinutes = calculateReadingTime(post.components);
   return (
     <Link href={`/blog/${post.slug}`} key={post.slug} className={`mb-6 flex lg:h-[300px] grow border-[3px] border-foreground group flex-col-reverse ${align === "left" ? "lg:flex-row" : "lg:flex-row-reverse"}`}>
       <div className={`p-4 group-hover:bg-foreground group-hover:text-background transition-colors grow ${align === "left" ? "text-left" : "text-right"} flex flex-col`}>
         <div>
           <SectionTitle>{post.title}</SectionTitle>
           <p className="text-lg mb-2">{post.description}</p>
-          <p className="text-sm font-condensed">
-            {post.date}
-            {readingTimeMinutes ? <> · ~{readingTimeMinutes} min read</> : null}
-          </p>
+          <p className="text-sm font-condensed">{post.date}</p>
         </div>
         {post.tags && post.tags.length > 0 && (
           <div className={`mt-auto flex flex-wrap gap-2 ${align === "left" ? "justify-start" : "justify-end"}`}>
